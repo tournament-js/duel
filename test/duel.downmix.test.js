616175060944b6c731c17322e616ba4be6525832
clux <analsandblaster@gmail.com>
2015-10-23 22:05:27 +0100

    more tests for downMix - finals are sensible in all cases

--- test/duel.downmix.test.js
+++ test/duel.downmix.test.js
@@ -24,12 +24,15 @@ test('downMix', function T(t) {
   var d = new Duel(16, { last: LB, short: true });
 
   d.matches.forEach(function (g) {
-    t.ok(d.score(g.id, g.p[0] < g.p[1] ? [2,0] : [0, 2]), 'could score ' + g.id);
+    d.score(g.id, g.p[0] < g.p[1] ? [2,0] : [0, 2]);
   });
 
   dm.matches.forEach(function (g) {
-    t.ok(dm.score(g.id, g.p[0] < g.p[1] ? [2,0] : [0, 2]), 'could score ' + g.id);
+    dm.score(g.id, g.p[0] < g.p[1] ? [2,0] : [0, 2]);
   });
+
+  t.ok(dm.isDone(), 'dm is done');
+  t.ok(d.isDone(), 'd is done');
 
   t.deepEqual(dm.findMatches({s: 1}), d.findMatches({s: 1}), 'wb identical');
   t.deepEqual(dm.findMatches({s: 2, r: 1}), d.findMatches({s: 2, r: 1}), 'lbr1 identical');
@@ -45,8 +48,30 @@ test('downMix', function T(t) {
       t.ok($.difference(mm.p, m.p).length >= 1, 'mixed up ' + mm.id);
     }
   });
+});
 
-
-  t.ok(dm.isDone(), 'dm is done');
-  t.ok(d.isDone(), 'd is done');
+/**
+ * General check that finals are handled correctly when using downMix
+ *
+ * Will verify the number of finals, and that all variations are sensible.
+ */
+test('downMixFinals', function T(t) {
+  [false, true].forEach(function (underDogWins) {
+    [false, true].forEach(function (short) {
+      var d = new Duel(16, { last: LB, downMix: true, short: short });
+      d.matches.forEach(function (g) {
+        if (d.unscorable(g.id, [1,0])) { // avoid log in gf2
+          return; // should be grand final game only (otherwise dm.isDone fails later)
+        }
+        d.score(g.id, underDogWins ? [0,2] : [2,0]);
+      });
+      var finals = d.findMatchesRanged({s: 2, r: 7}, {s: 2, r: 8});
+      t.equal(finals.length, 1+Number(!short), 'number of finals when short is '+ short);
+      if (!short) {
+        var gf2 = finals[1];
+        t.equal(d.isPlayable(gf2), underDogWins, 'gf2 only playable if underDogWins');
+      }
+      t.ok(d.isDone(), 'd is done');
+    });
+  });
 });
